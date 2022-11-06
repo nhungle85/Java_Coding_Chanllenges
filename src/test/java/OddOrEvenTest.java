03cd392a172c661b1005cc8fc6fb8379c3fb0dc0
nhungle <misssg85@gmail.com>
2022-11-06 04:48:29 -0600

    Add Class OddOrEven

---
+++ src/test/java/OddOrEvenTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+public class OddOrEvenTest {
+    @Test
+    public void isEven_true() {
+        assertTrue(OddOrEven.isEven(0));
+        assertTrue(OddOrEven.isEven(2));
+        assertTrue(OddOrEven.isEven(-2));
+    }
+
+    @Test
+    public void isEven_fail() {
+        assertFalse(OddOrEven.isEven(1));
+        assertFalse(OddOrEven.isEven(-1));
+        assertFalse(OddOrEven.isEven(93));
+
+    }
+}
