e1cb7fa5c4fe25669502c61b9d3b51d50ad56551
nhungle <misssg85@gmail.com>
2022-11-06 05:00:44 -0600

    Modify OddOrEven

--- src/main/java/OddOrEven.java
+++ src/main/java/OddOrEven.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class OddOrEven {
 
     static boolean isEven(int n) {
+        //return (n & 1) == 0 --> faster but hard to read
         return n%2 == 0;
     }
     public static void main(String[] args) {
-        System.out.println(isEven(5));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int userNum = scanner.nextInt();
+        System.out.println("Is the number even? " + isEven(userNum));
+
     }
 }
